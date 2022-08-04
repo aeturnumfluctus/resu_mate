30ec7ecdcc1992ec8f53ebc0391d3a952ed551ca
Josh Adams <joshuascottadams@gmail.com>
2022-08-04 05:59:39 -0400

    First!

---
+++ lib/resu_mate.ex
@@ -0,0 +1,18 @@
+defmodule ResuMate do
+  @moduledoc """
+  Documentation for `ResuMate`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ResuMate.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/resu_mate_test.exs
@@ -0,0 +1,8 @@
+defmodule ResuMateTest do
+  use ExUnit.Case
+  doctest ResuMate
+
+  test "greets the world" do
+    assert ResuMate.hello() == :world
+  end
+end
