320ab761f965b068546bab8c8ff14673e70cdc07
Josh Adams <joshuascottadams@gmail.com>
2022-08-04 06:09:10 -0400

    Extract version into module attr

--- mix.exs
+++ mix.exs
@@ -1,10 +1,12 @@
 defmodule ResuMate.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+
   def project do
     [
       app: :resu_mate,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps: deps()
